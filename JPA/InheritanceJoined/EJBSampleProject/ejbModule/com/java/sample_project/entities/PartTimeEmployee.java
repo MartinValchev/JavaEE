f10d33494614dca0e97b2ed1efabbde7fd152285
Martin Valchev <martin.valchev@esg.musala.com>
2019-01-31 14:48:58 +0200

    added Inheritance strategy joined example

---
+++ JPA/InheritanceJoined/EJBSampleProject/ejbModule/com/java/sample_project/entities/PartTimeEmployee.java
@@ -0,0 +1,35 @@
+package com.java.sample_project.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({@NamedQuery(name="PartTimeEmployee.findAll", query="select e from PartTimeEmployee e")})
+@Table(name="CH04_JOIN_PART_TIME_EMPLOYEE")
+public class PartTimeEmployee extends Employee implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1108897904475547993L;
+	@Column(name="HOURLY_WAGE")
+	private double hourlyWage;
+	 public PartTimeEmployee() {}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public double getHourlyWage() {
+		return hourlyWage;
+	}
+	public void setHourlyWage(double hourlyWage) {
+		this.hourlyWage = hourlyWage;
+	}
+	 
+	 
+	
+}
